c9962b4ae456f9d4d5358a8c4e3e5c51f3a4d44e
Rahman Khan M <55700950+RahmanKhan2932@users.noreply.github.com>
2022-02-15 09:55:11 +0530

    Add files via upload

---
+++ NewLead.java
@@ -0,0 +1,32 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class NewLead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		String title=driver.getTitle();
+		System.out.println(title);
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TCS");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Sam");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("M");
+		driver.findElement(By.name("dataSourceId")).sendKeys("R");
+		driver.findElement(By.name("submitButton")).click();
+
+	}
+
+}
